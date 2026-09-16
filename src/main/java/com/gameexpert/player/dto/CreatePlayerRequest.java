734a31aec4047cdc7099824ddbf34c2f63725e4f
psj45633 <psj45633@gmail.com>
2026-09-16 19:03:26 +0900

    feat : lv1~lv4
    Docker로 MySQL과 Redis 설정
    SQL을 JPA인덱스로 표현하기
    요청 검증과 DTO: 플레이어 등록
    월드 생성

--- src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
+++ src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
@@ -1,11 +1,16 @@
 package com.gameexpert.player.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class CreatePlayerRequest {
 
     // TODO Lv 3: 2~12글자의 영문 대소문자, 숫자와 밑줄을 허용하는 검증을 적용합니다.
+    @NotBlank
+    @Size(min =2, max=12)
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$")
     private final String nickname;
 
     public CreatePlayerRequest(String nickname) {
--- src/main/java/com/gameexpert/player/service/PlayerService.java
+++ src/main/java/com/gameexpert/player/service/PlayerService.java
@@ -20,7 +20,14 @@ public class PlayerService {
     @Transactional
     public void createPlayer(CreatePlayerRequest request) {
         // TODO Lv 3: 닉네임 중복을 확인하고 플레이어를 저장합니다.
-        throw new UnsupportedOperationException("Lv 3: 플레이어 등록을 구현하세요.");
+        if(playerRepository.existsByNickname(request.getNickname())){
+            throw new ConflictException("DUPLICATE_NICKNAME");
+        }
+
+
+        savePlayer(new Player(request.getNickname()));
+
+//        throw new UnsupportedOperationException("Lv 3: 플레이어 등록을 구현하세요.");
     }
 
     private void savePlayer(Player player) {
