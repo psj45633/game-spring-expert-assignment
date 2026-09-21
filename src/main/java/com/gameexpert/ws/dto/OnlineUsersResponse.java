019579683d1c1f4ce9202a259fc972b9f05bf8a2
psj45633 <psj45633@gmail.com>
2026-09-21 09:16:04 +0900

    feat : 접속자 목록 조회
    lv15

--- src/main/java/com/gameexpert/ws/dto/OnlineUsersResponse.java
+++ src/main/java/com/gameexpert/ws/dto/OnlineUsersResponse.java
@@ -6,6 +6,15 @@ import lombok.Getter;
 @Getter
 public class OnlineUsersResponse {
     // TODO Lv 15: API 명세에 맞게 응답 필드와 생성자를 완성합니다.
+    private String type;
+    private List<String> users;
+    private Integer count;
+
+
+
     public OnlineUsersResponse(List<String> users, int count) {
+        type = "onlineUsers";
+        this.users = users;
+        this.count = count;
     }
 }
--- src/main/java/com/gameexpert/ws/handler/OnlineUsersWsHandler.java
+++ src/main/java/com/gameexpert/ws/handler/OnlineUsersWsHandler.java
@@ -1,6 +1,10 @@
 package com.gameexpert.ws.handler;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import com.gameexpert.api.SessionRegistry;
 import com.gameexpert.ws.NicknameHandshakeInterceptor;
 import com.gameexpert.ws.WorldBroadcaster;
 import com.gameexpert.ws.WorldSessionRegistry;
@@ -24,5 +28,23 @@ public class OnlineUsersWsHandler implements WsMessageHandler {
     @Override
     public void handle(WsMessageContext context, JsonNode message) {
         // TODO Lv 15: 현재 월드의 열린 연결에서 닉네임을 조회하고 요청자에게 응답합니다.
+        Collection<SessionRegistry.Entry> entries = registry.entries(context.worldId());
+
+        List<String> nicknames = new ArrayList<>();
+
+        for(SessionRegistry.Entry entry : entries){
+            if(entry.session().isOpen()){
+                String nickname = (String)entry.session().getAttributes().get(NicknameHandshakeInterceptor.ATTR_NICKNAME);
+                nicknames.add(nickname);
+
+            }
+        }
+        nicknames.sort(String::compareTo);
+
+        OnlineUsersResponse response = new OnlineUsersResponse(nicknames,nicknames.size());
+
+        broadcaster.sendTo(context.session(), response);
+
+
     }
 }
