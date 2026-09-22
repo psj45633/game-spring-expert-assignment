ffd7bd53605556c015c3abcac5edeca12eb15788
psj45633 <psj45633@gmail.com>
2026-09-22 17:15:55 +0900

    feat : 멀티 서버
    lv20

--- src/main/java/com/gameexpert/chat/relay/ChatRelay.java
+++ src/main/java/com/gameexpert/chat/relay/ChatRelay.java
@@ -1,6 +1,9 @@
 package com.gameexpert.chat.relay;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+
+import com.gameexpert.ws.dto.ChatResponse;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -22,10 +25,24 @@ public class ChatRelay implements MessageListener {
 
     public void publish(Long worldId, Object message) {
         // TODO Lv 20: worldId와 message를 JSON으로 묶어 채팅 채널에 발행합니다.
+        Map<String,Object> payload = Map.of("worldId", worldId,"message",message);
+        String json = objectMapper.writeValueAsString(payload);
+        redisTemplate.convertAndSend(CHANNEL,json);
+
     }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
         // TODO Lv 20: JSON에서 worldId와 message를 읽어 localChatSender.send()로 전달합니다.
+        String json = new String(message.getBody(), StandardCharsets.UTF_8);
+        JsonNode root = objectMapper.readTree(json);
+
+        Long worldId = root.get("worldId").asLong();
+        JsonNode chatMessage = root.get("message");
+
+        ChatResponse response = objectMapper.treeToValue(chatMessage, ChatResponse.class);
+
+        localChatSender.send(worldId, chatMessage);
+
     }
 }
--- src/main/java/com/gameexpert/chat/service/ChatRateLimitService.java
+++ src/main/java/com/gameexpert/chat/service/ChatRateLimitService.java
@@ -39,16 +39,9 @@ public class ChatRateLimitService {
 
     public boolean allow(Long playerId) {
         String key = "chat:limit:" + playerId;
-//        String value = redisTemplate.opsForValue().get(key);
-//        int count = value == null ? 0 : Integer.parseInt(value);
-//        if (count >= 5) {
-//            return false;
-//        }
-//        // TODO Lv 19: 횟수 확인부터 최초 만료 설정까지 원자적으로 실행합니다.
-//        Long updated = redisTemplate.opsForValue().increment(key);
-//        if (updated == 1L) {
-//            redisTemplate.expire(key, Duration.ofSeconds(10));
-//        }
+
+        // TODO Lv 19: 횟수 확인부터 최초 만료 설정까지 원자적으로 실행합니다.
+
 
         Long result = redisTemplate.execute(
                 RATE_LIMIT_SCRIPT,
